2ae1863a5c52c9bbfc5c85484b1eb70039f8a896
hamer-web96 <berhehermela155@gmail.com>
2026-08-14 05:56:57 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 2;
+let z = x * y;
+document.getElementById("demo1").innerHTML = z;
+function add(x,y){
+    
+   
+    document.getElementById("demo").innerHTML= "sum="+ (x+y);
+}
+add(5,35);
+function input(){
+    document.getElementById("demo2").innerHTML="Name="+();
+}
+input(semu);
